b2736aa9d1ff2b1165e39fa984401282d0202780
CodeWithOric <codewithoric@gmail.com>
2025-06-14 15:37:24 +0630

    feat: login and registeration form

--- index.php
+++ index.php
@@ -1,11 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sunset Valley Hotel</title>
+    <title>Sunset Valley || Hotel</title>
+
+    <!-- --- Bootstrap Css link--- -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- --- Main css link --- -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- --- Poppins Font link --- -->
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
 </head>
+
 <body>
-    
+
+    <!-- Navbar Bar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white px-lg-3 py-lg-2 shadow-sm sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand me-5 fw-bold fs-3" href="index.php">Sunset Valley</a>
+            <button class="navbar-toggler shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Rooms</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Facilities</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active me-2" aria-current="page" href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <form action="login.php">
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+
+    <!-- --- Bootstrap Js --- -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- --- Main Js --- -->
+    <script src="js/script.js"></script>
 </body>
+
 </html>
